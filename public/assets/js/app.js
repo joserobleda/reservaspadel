5c9f1b41b9a732592129c2be92d625def52ab142
Janayama <javier.anaya.martinez@gmail.com>
2014-02-04 18:49:28 +0100

    funcion window

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,18 +1,20 @@
-function success(position) {
-	var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+(function(w, undefined) {
+	function success(position) {
+		var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-	var options = {
-		zoom: 15,
-		center: coords,
-		mapTypeControl: false,
-		navigationControlOptions: {
-			style: google.maps.NavigationControlStyle.SMALL
-		},
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("background"), options);
-}
+		var options = {
+			zoom: 15,
+			center: coords,
+			mapTypeControl: false,
+			navigationControlOptions: {
+				style: google.maps.NavigationControlStyle.SMALL
+			},
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("background"), options);
+	}
 
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(success);
-}
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(success);
+	}
+})(window);
