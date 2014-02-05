de9801920500668e1f78cad890be2bc022be32a0
Janayama <javier.anaya.martinez@gmail.com>
2014-02-05 23:36:12 +0100

    render effects y sacar nombre city

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,19 @@
 (function(w, undefined) {
-	function success(position) {
-		var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-		var options = {
+	function render(city) {
+		$('#mainsearch').val(city);
+		$('#main').animate({top:'0'}, 'slow', function () {
+			$('#results').show().animate({ height: "450px" }, 1500);
+		});
+
+	}
+
+	function success(position) {
+		var coord, map, latlng, coords, geocoder, city;
+
+		coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+		options = {
 			zoom: 15,
 			center: coords,
 			mapTypeControl: false,
@@ -11,10 +22,22 @@
 			},
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		var map = new google.maps.Map(document.getElementById("background"), options);
-	}
+
+		map 		= new google.maps.Map(document.getElementById("background"), options);
+		latlng 		= new google.maps.LatLng(coords['d'], coords['e']);
+		geocoder 	= new google.maps.Geocoder();
+
+		geocoder.geocode({'latLng': latlng}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					city = results[0].address_components[2].long_name;
+					render(city);
+				}
+			}
+		});
+	};
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(success);
-	}
+	};
 })(window);
