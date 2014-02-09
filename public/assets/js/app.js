eb49e4274ce0cf6116c627adaf07588083d1bf9a
Janayama <javier.anaya.martinez@gmail.com>
2014-02-09 16:52:35 +0100

    autocomplete

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -43,4 +43,15 @@
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(success);
 	};
+
+	var cities = ["Almería","Roquetas de Mar","Cádiz","Algeciras","Chiclana de la Frontera","Jerez de la Frontera","Puerto de Santa María","San Fernando","San Roque","Córdoba","Granada","Atarfe","Huelva","Aljaraque","Jaén","Málaga","Benahavís","Benalmádena","Estepona","Marbella","Sevilla","Alcalá de Guadaíra","Dos Hermanas","Espartinas","Mairena del Aljarafe","Rinconada","Huesca","Teruel","Zaragoza","Oviedo","Gijón","Palma de Mallorca","Palmas de Gran Canaria","Santa Cruz de Tenerife","Adeje","Santander","Ávila","Burgos","León","Ponferrada","Palencia","Salamanca","Segovia","Soria","Valladolid","Zamora","Albacete","Ciudad Real","Cuenca","Guadalajara","Toledo","Barcelona","Castellar del Vallés","Castelldefels","Mataró","Sabadell","Sant Cugat del Vallés","Terrassa","Viladecans","Girona","Lleida","Tarragona","Cambrils","Reus","Ceuta","Madrid","Alcalá de Henares","Alcobendas","Alcorcón","Aranjuez","Arganda del Rey","Boadilla del Monte","Fuenlabrada","Leganés","Majadahonda","Móstoles","Parla","Pozuelo de Alarcón","Rivas-Vaciamadrid","Rozas de Madrid","San Sebastián de los Reyes","Torrejón de Ardoz","Tres Cantos","Villanueva de la Cañada","Villaviciosa de Odón","Alicante","Benidorm","Elx","Orihuela","Sant Joan d Alacant","Castellón de la Plana","Valencia","el Torrent","Eliana","Paterna","Sagunto/Sagunt","Xàtiva","Badajoz","Mérida","Cáceres","La Coruña","Lugo","Ourense","Pontevedra","Vigo","Logroño","Melilla", "Pamplona Iruña","Vitoria-Gasteiz","San Sebastián","Bilbao","Getxo","Murcia","Cartagena","Molina de Segura"];
+	 
+	$('#mainsearch').typeahead({local: cities})
+	.on('typeahead:selected', function(e){
+     	$.post("/places", { city: $(this).val() }).done(function(res) { 
+			$('#results').html(res);
+		});
+   });
+
+
 })(window);
