7ee5346f244ea1b146fddaa5e4be0cdf75fbfd13
Jose Ignacio Andres <joserobleda@gmail.com>
2014-03-19 17:58:55 +0100

    search by near

--- controllers/place.js
+++ controllers/place.js
@@ -6,45 +6,63 @@
 
 
 	Place.getPlaces = function (req, res, next) {
-		var city 		= req.body.city;
-		var search 		= {'city.title': new RegExp(city, 'i')};
-		var coords 		= req.body.coords;
-		var places 		= Place.find(search);
-		var hasCoords 	= true;
+		var search, hasCoords, places, city, coords;
+
+
+		city 		= req.body.city;		
+		coords 		= req.body.coords
 
 		req.trackEvent('submit', city);
 
 		if (coords) {
+			hasCoords = true;
+
 			req.trackEvent('origin', coords.lat + ',' + coords.lng);
 
 			// coords to compare
 			coords = {
-				latitude: coords.lat,
-				longitude: coords.lng
+				latitude: parseFloat(coords.lat),
+				longitude: parseFloat(coords.lng)
 			};
+
+			search = {
+				point: { 
+					$near: [coords.latitude, coords.longitude],
+					$maxDistance: 10000
+				},
+
+			};
+
 		} else {
-			hasCoords = false;
+			hasCoords 	= false;
+			search 		= {'city.title': new RegExp(city, 'i')};
 		}
 
+
+		var places = Place.find(search).fail(function (err) {
+			console.log(err);
+		});
 
 		
 		coords = coords ? coords : maps.getCoords(city);
 
 		Q.all([places, coords]).spread(function (places, coords) {
-			// called to sort
-			places.comparator = function (place) {
-				return place.get('distance').meters;
-			};
-
-
 			// create the distance prop
 			places.each(function (place) {
 				var distance = place.getDistance(coords);
 				place.set('distance', distance);
 			});
 
-			// sort by distance
+			
+			// called to sort
+			places.comparator = function (place) {
+				return place.get('distance').meters;
+			};
+
+
 			places.sort();
+			
+
 			// render the view 
 			res.render('places.twig', {places: places.toJSON().slice(0, 20), city: city, hasCoords: hasCoords});
 		});
